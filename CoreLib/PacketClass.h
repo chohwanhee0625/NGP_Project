1c7ba12900ca82e1df4f11feffe6671942baece1
ji <qq00aa@naver.com>
2024-11-02 19:05:01 +0900

    add Packtclass.h
    add Packtclass.h

--- CoreLib/PacketClass.h
+++ CoreLib/PacketClass.h
@@ -1,28 +1,86 @@
 #pragma once
 #include "include.h"
 
-class PacketClass
+class INIT_DATA_P
 {
-	int x, y, z;
+private:
 
-	std::string to_json()
-	{
-		json jObject;
-		jObject["x"] = x;
-		jObject["y"] = y;
-		jObject["z"] = z;
+	bool	Player_ID;
+	float	Player_Pos_x;
+	float	Player_Pos_y;
+	float	Player_Pos_z;
 
-		std::string j_str = jObject.dump(4);
+public:	
 
-		return j_str;
-	}
+	std::string to_json();
 
-	void from_json(std::string& j_str)
-	{
-		auto jObject = json::parse(j_str);
-		x = jObject.at("x").get<int>();
-		y = jObject.at("y").get<int>();
-		z = jObject.at("z").get<int>();
-	}
+	void from_json(std::string& j_str);
+	
 };
+
+
+
+class INIT_DATA_R
+{
+private:
+	std::vector<bool> Roads_Flags;
+	std::vector<bool> Dir_Flags;
+
+public:
+	std::string to_json();
+
+	void from_json(std::string& j_str);
+
+};
+
+
+class INIT_DATA_C
+{
+private:
+	std::vector<float>	Cars_Velocity;
+	std::vector<std::array<float, 3>> Cars_Color_RGB;
+
+
+public:
+	std::string to_json();
+
+	void from_json(std::string& j_str);
+
+};
+
+
+class INIT_DATA_W
+{
+private:
+
+	std::vector<bool> Woods_Flags;
+
+public:
+	std::string to_json();
+
+	void from_json(std::string& j_str);
+
+};
+
+
+
+class UPDATE_DATA
+{
+private:
+	bool	Player_ID;
+	float	Other_Player_Pos_x;
+	float	Other_Player_Pos_y;
+	float	Other_Player_Pos_z;
+	char	Other_Player_Face;
+	bool	GameOver_Flag;
+
+
+public:
+	std::string to_json();
+
+	void from_json(std::string& j_str);
+	
+};
+
+
 
