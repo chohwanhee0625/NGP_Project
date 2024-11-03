34f5d65117e89988fa50dce1d6c91e3071c11dac
gogwangsin <gwangsin1029@naver.com>
2024-11-03 18:40:16 +0900

    변수명 통일

--- ComeBackHome/Camera.cpp
+++ ComeBackHome/Camera.cpp
@@ -7,105 +7,115 @@
 
 void Camera::PerspectiveProjection()
 {
-	Total_projection = glm::perspective(glm::radians(45.0f), 1.f, 0.1f, 50.f);
-	Total_projection = glm::translate(Total_projection, glm::vec3(0.0f, 0.0f, 0.f));
+	total_projection = glm::perspective(glm::radians(45.0f), 1.f, 0.1f, 50.f);
+	total_projection = glm::translate(total_projection, glm::vec3(0.0f, 0.0f, 0.f));
 	unsigned int projectionLocation = glGetUniformLocation(gShaderProgramID, "projectionTransform");
-	glUniformMatrix4fv(projectionLocation, 1, GL_FALSE, glm::value_ptr(Total_projection));
+	glUniformMatrix4fv(projectionLocation, 1, GL_FALSE, glm::value_ptr(total_projection));
 }
 
-void Camera::setViewTransform_firstVeiwPort()
+void Camera::SetViewTransform_ThirdPersonViewport()
 {
-	Total_view = glm::mat4{ 1.0f };
+	total_view = glm::mat4{ 1.0f };
 	
-	if (gToggle[(int)Toggle::NearFar] == Far)
-		cameraPos = glm::vec3{ 0.1f + camera_move_x , 0.5f + camera_move_y, 0.5f + camera_move_z };
-	else if (gToggle[(int)Toggle::NearFar] == Near)
-		cameraPos = glm::vec3{ 0.0f + camera_move_x , camera_move_y + 0.05f, 0.2f + camera_move_z }; // y-0.05f
+	if (gToggle[(int)Toggle::NearFar] == Far) {
+		// 먼 3인칭
+		camera_pos = glm::vec3{ 0.1f + camera_move_x , 0.5f + camera_move_y, 0.5f + camera_move_z };
+	}
+	else if (gToggle[(int)Toggle::NearFar] == Near) {
+		// 가까운 3인칭
+		camera_pos = glm::vec3{ 0.0f + camera_move_x , camera_move_y + 0.05f, 0.2f + camera_move_z }; // y-0.05f
+	}
 	
-	cameraDirection = glm::vec3{ 0.0f + camera_move_x, 0.0f + camera_move_y, 0.0f + camera_move_z };
+	camera_direction = glm::vec3{ 0.0f + camera_move_x, 0.0f + camera_move_y, 0.0f + camera_move_z };
 	
-	cameraUp = glm::vec3{ 0.f, 1.f, 0.f };
-	Total_view = glm::lookAt(cameraPos, cameraDirection, cameraUp);
+	camera_up = glm::vec3{ 0.f, 1.f, 0.f };
+	total_view = glm::lookAt(camera_pos, camera_direction, camera_up);
 
 	unsigned int viewLocation = glGetUniformLocation(gShaderProgramID, "viewTransform");
-	glUniformMatrix4fv(viewLocation, 1, GL_FALSE, glm::value_ptr(Total_view));
+	glUniformMatrix4fv(viewLocation, 1, GL_FALSE, glm::value_ptr(total_view));
 }
 
-void Camera::setViewTransform_firstVeiwPort_One()
+void Camera::SetViewTransform_FirstPersonViewport()
 {
-	Total_view = glm::mat4{ 1.0f };
+	// 1인칭 
+
+	total_view = glm::mat4{ 1.0f };
 	
 	if (North == face) {
-		cameraPos = glm::vec3{ camera_move_x , camera_move_y+0.015, camera_move_z };
-		cameraDirection = glm::vec3{camera_move_x, camera_move_y, -5.f + camera_move_z };
+		camera_pos = glm::vec3{ camera_move_x , camera_move_y+0.015, camera_move_z };
+		camera_direction = glm::vec3{camera_move_x, camera_move_y, -5.f + camera_move_z };
 	}
 	else if (South == face) {
-		cameraPos = glm::vec3{ camera_move_x , camera_move_y + 0.015, camera_move_z };
-		cameraDirection = glm::vec3{ camera_move_x, camera_move_y, +5.f + camera_move_z };
+		camera_pos = glm::vec3{ camera_move_x , camera_move_y + 0.015, camera_move_z };
+		camera_direction = glm::vec3{ camera_move_x, camera_move_y, +5.f + camera_move_z };
 	}
 	else if (East == face) {
-		cameraPos = glm::vec3{ camera_move_x , camera_move_y + 0.015, camera_move_z };
-		cameraDirection = glm::vec3{ 5.f+camera_move_x, camera_move_y,  + camera_move_z };
+		camera_pos = glm::vec3{ camera_move_x , camera_move_y + 0.015, camera_move_z };
+		camera_direction = glm::vec3{ 5.f+camera_move_x, camera_move_y,  + camera_move_z };
 	}
 	else if (West == face) {
-		cameraPos = glm::vec3{ camera_move_x , camera_move_y + 0.015, camera_move_z };
-		cameraDirection = glm::vec3{ camera_move_x - 5.f, camera_move_y, + camera_move_z };
+		camera_pos = glm::vec3{ camera_move_x , camera_move_y + 0.015, camera_move_z };
+		camera_direction = glm::vec3{ camera_move_x - 5.f, camera_move_y, + camera_move_z };
 	}
 	else
 	{
-		if (North == Past_face) {
-			cameraPos = glm::vec3{ camera_move_x , camera_move_y + 0.01, camera_move_z };
-			cameraDirection = glm::vec3{ camera_move_x, camera_move_y, -5.f + camera_move_z };
+		if (North == pre_face) {
+			camera_pos = glm::vec3{ camera_move_x , camera_move_y + 0.01, camera_move_z };
+			camera_direction = glm::vec3{ camera_move_x, camera_move_y, -5.f + camera_move_z };
 		}
-		else if (South == Past_face) {
-			cameraPos = glm::vec3{ camera_move_x , camera_move_y + 0.01, camera_move_z };
-			cameraDirection = glm::vec3{ camera_move_x, camera_move_y, +5.f + camera_move_z };
+		else if (South == pre_face) {
+			camera_pos = glm::vec3{ camera_move_x , camera_move_y + 0.01, camera_move_z };
+			camera_direction = glm::vec3{ camera_move_x, camera_move_y, +5.f + camera_move_z };
 		}
-		else if (East == Past_face) {
-			cameraPos = glm::vec3{ camera_move_x , camera_move_y + 0.01, camera_move_z };
-			cameraDirection = glm::vec3{ 5.f + camera_move_x, camera_move_y,  +camera_move_z };
+		else if (East == pre_face) {
+			camera_pos = glm::vec3{ camera_move_x , camera_move_y + 0.01, camera_move_z };
+			camera_direction = glm::vec3{ 5.f + camera_move_x, camera_move_y,  +camera_move_z };
 		}
-		else if (West == Past_face) {
-			cameraPos = glm::vec3{ camera_move_x , camera_move_y + 0.01, camera_move_z };
-			cameraDirection = glm::vec3{ camera_move_x - 5.f, camera_move_y, +camera_move_z };
+		else if (West == pre_face) {
+			camera_pos = glm::vec3{ camera_move_x , camera_move_y + 0.01, camera_move_z };
+			camera_direction = glm::vec3{ camera_move_x - 5.f, camera_move_y, +camera_move_z };
 		}
 	}
 
-	cameraUp = glm::vec3{ 0.f, 1.f, 0.f };
-	Total_view = glm::lookAt(cameraPos, cameraDirection, cameraUp);
+	camera_up = glm::vec3{ 0.f, 1.f, 0.f };
+	total_view = glm::lookAt(camera_pos, camera_direction, camera_up);
 
 	unsigned int viewLocation = glGetUniformLocation(gShaderProgramID, "viewTransform");
-	glUniformMatrix4fv(viewLocation, 1, GL_FALSE, glm::value_ptr(Total_view));
+	glUniformMatrix4fv(viewLocation, 1, GL_FALSE, glm::value_ptr(total_view));
 }
 
-void Camera::setViewTransform_secondVeiwPort()
+void Camera::SetViewTransform_ChickenViewport()
 {
-	Total_view = glm::mat4{ 1.0f };
+	// 치킨 고정 카메라
+
+	total_view = glm::mat4{ 1.0f };
 
 	//   cameraPos = glm::vec3{ 0.1f + camera_move_x , 0.08f, -0.2f + camera_move_z };
  //   cameraPos = glm::vec3{ 0.05f + camera_move_x ,camera_move_y +  0.08f, -0.07f + camera_move_z };
-	cameraPos = glm::vec3{ 0.05f + camera_move_x ,camera_move_y + 0.1f, -0.07f + camera_move_z };
+	camera_pos = glm::vec3{ 0.05f + camera_move_x ,camera_move_y + 0.1f, -0.07f + camera_move_z };
 
-	cameraDirection = glm::vec3{ 0.0f + camera_move_x, camera_move_y + 0.0f, 0.0f + camera_move_z };
-	cameraUp = glm::vec3{ 0.f, 1.f, 0.f };
-	Total_view = glm::lookAt(cameraPos, cameraDirection, cameraUp);
+	camera_direction = glm::vec3{ 0.0f + camera_move_x, camera_move_y + 0.0f, 0.0f + camera_move_z };
+	camera_up = glm::vec3{ 0.f, 1.f, 0.f };
+	total_view = glm::lookAt(camera_pos, camera_direction, camera_up);
 
 	unsigned int viewLocation = glGetUniformLocation(gShaderProgramID, "viewTransform");
-	glUniformMatrix4fv(viewLocation, 1, GL_FALSE, glm::value_ptr(Total_view));
+	glUniformMatrix4fv(viewLocation, 1, GL_FALSE, glm::value_ptr(total_view));
 }
 
-void Camera::setViewTransform_thirdVeiwPort()
+void Camera::SetViewTransform_BorderViewport()
 {
-	Total_view = glm::mat4{ 1.0f };
+	// 경계 액자 카메라
 
-	cameraPos = glm::vec3{ 0.0f, camera_move_y + 0.5f, 30.0f };
+	total_view = glm::mat4{ 1.0f };
 
-	cameraDirection = glm::vec3{ 0.0f, camera_move_y + 0.0f, 30.0f };
-	cameraUp = glm::vec3{ 0.f, 0.f, -1.f };
-	Total_view = glm::lookAt(cameraPos, cameraDirection, cameraUp);
+	camera_pos = glm::vec3{ 0.0f, camera_move_y + 0.5f, 30.0f };
+
+	camera_direction = glm::vec3{ 0.0f, camera_move_y + 0.0f, 30.0f };
+	camera_up = glm::vec3{ 0.f, 0.f, -1.f };
+	total_view = glm::lookAt(camera_pos, camera_direction, camera_up);
 
 	unsigned int viewLocation = glGetUniformLocation(gShaderProgramID, "viewTransform");
-	glUniformMatrix4fv(viewLocation, 1, GL_FALSE, glm::value_ptr(Total_view));
+	glUniformMatrix4fv(viewLocation, 1, GL_FALSE, glm::value_ptr(total_view));
 }
 
 void Camera::SetCameraFaceDir(unsigned char key)
@@ -134,7 +144,7 @@ void Camera::SetCameraFaceDir(unsigned char key)
 
 	case STOP:
 		if (STOP != face) {
-			Past_face = face;
+			pre_face = face;
 		}
 		face = STOP;
 		break;
@@ -143,9 +153,9 @@ void Camera::SetCameraFaceDir(unsigned char key)
 
 void Camera::Move()
 {
-		camera_move_z = gVec.at(0)->GetZpos();
 		camera_move_x = gVec.at(0)->GetXpos();
 		camera_move_y = gVec.at(0)->GetYpos();
+		camera_move_z = gVec.at(0)->GetZpos();
 	
 		/*switch (face)
 	{
--- ComeBackHome/Light.cpp
+++ ComeBackHome/Light.cpp
@@ -10,20 +10,20 @@ float Interpolation(float a, float b, float t) {
 	return a + t * (b - a);
 }
 
-void Light::initLight()
+void Light::InitLight()
 {
-	lightPos[0] = 5.f;
-	lightPos[1] = 15.f;
-	lightPos[2] = -7.f;
+	light_pos[0] = 5.f;
+	light_pos[1] = 15.f;
+	light_pos[2] = -7.f;
 
-	lightColor[0] = 1.f; 
-	lightColor[1] = 1.f;
-	lightColor[2] = 1.f;
+	light_color[0] = 1.f; 
+	light_color[1] = 1.f;
+	light_color[2] = 1.f;
 
 	unsigned int lightPosLocation = glGetUniformLocation(gShaderProgramID, "u_lightPos"); //--- lightPos 값 전달: (0.0, 0.0, 5.0);
-	glUniform3f(lightPosLocation, lightPos[0], lightPos[1], lightPos[2]);
+	glUniform3f(lightPosLocation, light_pos[0], light_pos[1], light_pos[2]);
 	unsigned int lightColorLocation = glGetUniformLocation(gShaderProgramID, "u_lightColor");  //--- lightColor 값 전달: (1.0, 1.0, 1.0) 백색
-	glUniform3f(lightColorLocation, lightColor[0], lightColor[1], lightColor[2]);
+	glUniform3f(lightColorLocation, light_color[0], light_color[1], light_color[2]);
 
 	// 아침: 밝은 파란 하늘 : (0.529, 0.808, 0.922)
 	//		옅은 분홍 또는 주황색 : (1.0, 0.843, 0.678)
@@ -35,17 +35,17 @@ void Light::initLight()
 	//		검은색 : (0.0, 0.0, 0.0
 }
 
-void Light::setTransform()
+void Light::SetTransform()
 {
-	Total_view = glm::mat4{ 1.0f };
+	total_view = glm::mat4{ 1.0f };
 
-	lightPos = glm::vec3{ 0.0f, 25.0f, 30.f};
+	light_pos = glm::vec3{ 0.0f, 25.0f, 30.f};
 
 	unsigned int viewLocation = glGetUniformLocation(gShaderProgramID, "viewTransform");
-	glUniformMatrix4fv(viewLocation, 1, GL_FALSE, glm::value_ptr(Total_view));
+	glUniformMatrix4fv(viewLocation, 1, GL_FALSE, glm::value_ptr(total_view));
 }
 
-void Light::update()
+void Light::Update()
 {
 	float chicken_z_distance = -(gVec[0]->GetZpos()); // 치킨 z거리 범위 0 ~ 15, -z가 앞으로 가는 것이기에 -로 양수화
 
@@ -53,7 +53,7 @@ void Light::update()
 		// 거리 7스텝으로 나눠서 보간 ( 선형보간에서 t 범위 0 ~ 1 ) - 1차 보간 
 		// 0부터 7까지 z범위를 0.0~1.0으로 만들어서 t 범위로 사용
 		sunset_t = chicken_z_distance / 7;
-		sunsetInterpolation(sunset_t);
+		SetSunsetInterpolation(sunset_t);
 
 //		cout << "chicken_z, sunset_t, R,G,B : " << chicken_z_distance << ", " << sunset_t << ", " << lightColor[0] << ", " << lightColor[1] << ", " << lightColor[2] << "\n";
 	}
@@ -61,26 +61,26 @@ void Light::update()
 		// 보간 시작 t값을 0으로 초기화 - 2차 보간 
 		// sunset_t가 1.0몇이기 때문에 새로운 night_t를 구하되 -sunset_t(1.f)를 빼서 night_t(0.0 ~ 1.0) 범위 지키기
 		night_t = chicken_z_distance / 7 - sunset_t; 
-		nightInterpolation(night_t);
+		SetNightInterpolation(night_t);
 		
 //		cout << "chicken_z, night_t, R,G,B : " << chicken_z_distance << ", " << night_t << ", " << lightColor[0] << ", " << lightColor[1] << ", " << lightColor[2] << "\n";
 	}
 
 	unsigned int lightColorLocation = glGetUniformLocation(gShaderProgramID, "u_lightColor");  //--- lightColor 값 전달: (1.0, 1.0, 1.0) 백색
-	glUniform3f(lightColorLocation, lightColor[0], lightColor[1], lightColor[2]);
+	glUniform3f(lightColorLocation, light_color[0], light_color[1], light_color[2]);
 }
 
-void Light::sunsetInterpolation(float t) // 노을 보간 
+void Light::SetSunsetInterpolation(float t) // 노을 보간 
 {
 	// 시작 RGB, 끝 RGB, 보간t(0.0~1.0)
-	lightColor[0] = Interpolation(1., 1.0f, t); 
-	lightColor[1] = Interpolation(1., 0.4f, t);
-	lightColor[2] = Interpolation(1., 0.5f, t);
+	light_color[0] = Interpolation(1., 1.0f, t); 
+	light_color[1] = Interpolation(1., 0.4f, t);
+	light_color[2] = Interpolation(1., 0.5f, t);
 }
 
-void Light::nightInterpolation(float t) // 밤 보간
+void Light::SetNightInterpolation(float t) // 밤 보간
 {
-	lightColor[0] = Interpolation(1.0f, 0.3, t);
-	lightColor[1] = Interpolation(0.4f, 0.3, t);
-	lightColor[2] = Interpolation(0.5f, 0.5, t);
+	light_color[0] = Interpolation(1.0f, 0.3, t);
+	light_color[1] = Interpolation(0.4f, 0.3, t);
+	light_color[2] = Interpolation(0.5f, 0.5, t);
 }
