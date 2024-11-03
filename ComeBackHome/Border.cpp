7f57c7ff6e6415bc524706c99b1df2eed8c5425a
gogwangsin <gwangsin1029@naver.com>
2024-11-03 18:39:38 +0900

    주석 제거

--- ComeBackHome/Border.cpp
+++ ComeBackHome/Border.cpp
@@ -5,46 +5,13 @@
 
 //===========================================================================================
 
-void Border::initVertex(const GLfloat rec_array[36 * 6])
-{
-	for (int i = 0; i < 36 * 6; ++i) {
-		this->m_vertex[i] = rec_array[i];
-	}
-}
-
-void Border::initColor(const GLfloat color_array[36 * 3])
+void Border::InitColor(const GLfloat color_array[36 * 3])
 {
 	for (int i = 0; i < 36 * 3; i += 3) {
 		m_color[i + 0] = 1.f;
 		m_color[i + 1] = 0.50196f;
 		m_color[i + 2] = 0.70196f;
 	}
-}
-
-void Border::InitBuffer()
-{
-	glGenVertexArrays(1, &this->m_vao);
-	glBindVertexArray(this->m_vao);
-
-	glGenBuffers(1, &this->m_pos_vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, this->m_pos_vbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(this->m_vertex), this->m_vertex, GL_STATIC_DRAW);
-
-	int PosLocation = glGetAttribLocation(gShaderProgramID, "in_Position");
-	glVertexAttribPointer(PosLocation, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(PosLocation);
-
-	int NormalLocation = glGetAttribLocation(gShaderProgramID, "in_Normal");
-	glVertexAttribPointer(NormalLocation, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(NormalLocation);
-
-	glGenBuffers(1, &this->m_color_vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, this->m_color_vbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(this->m_color), this->m_color, GL_STATIC_DRAW);
-
-	int ColorLocation = glGetAttribLocation(gShaderProgramID, "in_Color");
-	glVertexAttribPointer(ColorLocation, 3, GL_FLOAT, GL_FALSE, 0, 0);
-
 }
 
 void Border::DrawObject()
--- ComeBackHome/Border.h
+++ ComeBackHome/Border.h
@@ -3,25 +3,19 @@
 
 class Border : public BasisComponent
 {
-private:
-	GLfloat m_vertex[36 * 6]; // Element로 그릴거면 4개, 아니면 6개 
-	GLfloat m_color[36 * 3]; // 108
-
 public:
 	Border()
 	{}
 	Border(const GLfloat cube_array[36 * 6], const GLfloat color_array[36 * 3])
 		: BasisComponent()
 	{
-		initVertex(cube_array);
-		initColor(color_array);
+		InitVertex(cube_array);
+		InitColor(color_array);
 		InitMatrix4();
 		InitBuffer();
 	}
 
-	void initVertex(const GLfloat rec_array[36 * 6]);
-	void initColor(const GLfloat color_array[36 * 3]);
-	void InitBuffer();
+	void InitColor(const GLfloat color_array[36 * 3]) override;
 	void DrawObject();
 	void InitMatrix4();
 
