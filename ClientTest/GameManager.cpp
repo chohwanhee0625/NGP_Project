163c298a109bb7cd87837c46e5ddf781486227bc
chohwanhee(lggram) <great1625@gmail.com>
2024-11-10 00:17:42 +0900

    Update GameManager.cpp

--- ClientTest/GameManager.cpp
+++ ClientTest/GameManager.cpp
@@ -7,9 +7,28 @@ void GameManager::WaitForOtherPlayer()
 
 	// recv Start Flag
 
-	std::thread NetworkThread(&GameManager::UpdateWorld, this, sock);
+	std::thread networkThread(&GameManager::UpdateWorld, this, sock);
+
+	networkThread.join();
 }
 
 void GameManager::UpdateWorld(SOCKET sock)
 {
+	using namespace std::chrono;
+
+	// recv world Data
+
+	while (true)
+	{
+		// send myplayer data
+
+		// recv otherplayer data
+
+		if (/* GameEndFlag == true */true)
+			break;
+
+		std::this_thread::sleep_for(1000ms / PACKET_FREQ);
+	}
+
+	// end Game Event
 }
