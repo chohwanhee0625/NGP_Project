d1bfd49d65184de727ea6ae516f5b8ae9edc8702
chohwanhee(lggram) <great1625@gmail.com>
2024-11-10 00:00:41 +0900

    WaitForOtherPlayer 준비

--- ClientTest/GameManager.cpp
+++ ClientTest/GameManager.cpp
@@ -1,1 +1,15 @@
 #include "GameManager.h"
+
+void GameManager::WaitForOtherPlayer()
+{
+	// connect
+	SOCKET sock;
+
+	// recv Start Flag
+
+	std::thread NetworkThread(&GameManager::UpdateWorld, this, sock);
+}
+
+void GameManager::UpdateWorld(SOCKET sock)
+{
+}
